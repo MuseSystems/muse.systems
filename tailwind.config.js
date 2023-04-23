a66b238af7e2272db7dc7b0b6b9d60b180f0c9df
Steven C. Buttgereit <github.com@musesystems.com>
2023-04-23 11:31:33 -0700

    Add tailwindcss and some related initial prep

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "content/**/*.md",
+    "layouts/**/*.html",
+    "themes/musetheme/layouts/**/*.html",
+    "themes/musetheme/content/**/*.md"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
