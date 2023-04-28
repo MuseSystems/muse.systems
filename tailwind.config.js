8c7c9820ddd8b9496f4db315d19ec473fd42bc4f
Steven C. Buttgereit <github.com@musesystems.com>
2023-04-28 14:30:51 -0700

    Final site commit prior to config and content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,97 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "content/**/*.md",
-    "layouts/**/*.html",
-    "themes/musetheme/layouts/**/*.html",
-    "themes/musetheme/content/**/*.md"],
+      "layouts/**/*.html",
+      "content/**/*.html",
+      "themes/musetheme/layouts/**/*.html",
+      "themes/musetheme/config.toml",
+      "themes/musetheme/content/**/*.html",
+      "themes/musetheme/assets/js/search.js"
+    ],
   theme: {
-    extend: {},
+    extend: {
+      typography: (theme) => ({
+        DEFAULT: {
+          css: [
+            {
+              'code::before': {
+                content: '""',
+              },
+              'code::after': {
+                content: '""',
+              },
+            }
+          ]
+        },
+        dark: {
+          css: [
+            {
+              color: theme("colors.gray.400"),
+              '[class~="lead"]': {
+                color: theme("colors.gray.300"),
+              },
+              a: {
+                color: theme("colors.white"),
+              },
+              strong: {
+                color: theme("colors.white"),
+              },
+              "ol > li::before": {
+                color: theme("colors.gray.400"),
+              },
+              "ul > li::before": {
+                backgroundColor: theme("colors.gray.600"),
+              },
+              hr: {
+                borderColor: theme("colors.gray.200"),
+              },
+              blockquote: {
+                color: theme("colors.gray.200"),
+                borderLeftColor: theme("colors.gray.600"),
+              },
+              h1: {
+                color: theme("colors.white"),
+              },
+              h2: {
+                color: theme("colors.white"),
+              },
+              h3: {
+                color: theme("colors.white"),
+              },
+              h4: {
+                color: theme("colors.white"),
+              },
+              "figure figcaption": {
+                color: theme("colors.gray.400"),
+              },
+              code: {
+                color: theme("colors.white"),
+              },
+              "a code": {
+                color: theme("colors.white"),
+              },
+              'code::before': {
+                content: '""',
+              },
+              'code::after': {
+                content: '""',
+              },
+              pre: {
+                color: theme("colors.gray.200"),
+                backgroundColor: theme("colors.gray.800"),
+              },
+              thead: {
+                color: theme("colors.white"),
+                borderBottomColor: theme("colors.gray.400"),
+              },
+              "tbody tr": {
+                borderBottomColor: theme("colors.gray.600"),
+              },
+            },
+          ],
+        },
+      }),
+    },
   },
-  plugins: [],
-}
-
+  plugins: [require("@tailwindcss/typography")],
+};
